51b6572e433cb41740775ddb8cf2f96432a93f87
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-03-10 11:05:12 +0530

    initial commit

---
+++ src/main/java/com/quickcart/component/payment/PaymentController.java
@@ -0,0 +1,23 @@
+package com.quickcart.component.payment;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/rest/v1/payments")
+public class PaymentController {
+
+    @Value("${payment.modes}")
+    private List<String> paymentModes;
+
+    @GetMapping("/modes")
+    public String getAvailablePaymentOptions() {
+        return paymentModes.get(new Random().nextInt(3 - 0 + 1) + 0);
+    }
+}
+
